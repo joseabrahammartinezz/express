b002b8332dcd39ca765702d28b7c679fa5bee3fe
José Abraham Martínez Soliz <jose.abraham.martinezz@gmail.com>
2022-09-08 21:27:36 -0400

    Manejo de errores

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,7 +1,9 @@
 const catchAsync = (fn) => {
-    return (req,res,next) => {
-        fn(req,res,next).cach((e) => {next(e)});
-    }
-}
-
-module.exports = catchAsync
+    return (req, res, next) => {
+      fn(req, res, next).catch((e) => {
+        next(e);
+      });
+    };
+  };
+  
+  module.exports = catchAsync;
